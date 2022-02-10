b29034ad884d081c4228dbeb94a3ce7e463e3e3c
邢恒 <xxingheng@163.com>
2022-02-10 16:45:44 +0800

    初始提交

---
+++ my-spring-01/src/main/java/BeanDefine.java
@@ -0,0 +1,21 @@
+/**
+ * @author xxhstart
+ * @create 2022-02-10 16:44
+ * @description:
+ */
+public class BeanDefine {
+
+    private Object bean;
+
+    public BeanDefine(Object bean) {
+        this.bean = bean;
+    }
+
+    public Object getBean() {
+        return bean;
+    }
+
+    public void setBean(Object bean) {
+        this.bean = bean;
+    }
+}
