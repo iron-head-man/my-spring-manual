e45feb59babd03b7ddfc77999ddf1e27d817bfbf
邢恒 <xxingheng@163.com>
2022-03-04 23:49:27 +0800

    【从Location读取bean定义注册】

--- my-spring-05/src/main/java/com/xiaoxing/springframwork05/beans/factory/support/AbstractBeanDefinitionReader.java
+++ my-spring-05/src/main/java/com/xiaoxing/springframwork05/beans/factory/support/AbstractBeanDefinitionReader.java
@@ -1,5 +1,8 @@
 package com.xiaoxing.springframwork05.beans.factory.support;
 
+import com.xiaoxing.springframwork05.core.io.DefaultResourceLoader;
+import com.xiaoxing.springframwork05.core.io.ResourceLoader;
+
 /**
  * <p>
  *
@@ -8,4 +11,27 @@ package com.xiaoxing.springframwork05.beans.factory.support;
  * @author heng.xing@hand-china.com 2022-03-02 22:52
  */
 public abstract class AbstractBeanDefinitionReader implements BeanDefinitionReader{
+
+
+    private final BeanDefinitionRegistry registry;
+
+    private ResourceLoader resourceLoader;
+
+    public AbstractBeanDefinitionReader(BeanDefinitionRegistry registry,ResourceLoader resourceLoader){
+        this.resourceLoader = resourceLoader;
+        this.registry = registry;
+    }
+    public AbstractBeanDefinitionReader(BeanDefinitionRegistry registry){
+        this(registry, new DefaultResourceLoader());
+    }
+
+    @Override
+    public BeanDefinitionRegistry getRegistry() {
+        return registry;
+    }
+
+    @Override
+    public ResourceLoader getResourceLoader() {
+        return resourceLoader;
+    }
 }
---
+++ my-spring-05/src/main/java/com/xiaoxing/springframwork05/beans/factory/xml/XmlBeanDefinitionReader.java
@@ -0,0 +1,11 @@
+package com.xiaoxing.springframwork05.beans.factory.xml;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author heng.xing@hand-china.com 2022-03-04 22:26
+ */
+public class XmlBeanDefinitionReader {
+}
